40751e6de520804901c39a5e60429abaf373d4ea
sia661 <41309363+sia661@users.noreply.github.com>
2018-10-13 21:50:19 +0800

    Add files via upload
    Tentative designs for Engineer's homepage and navbar

---
+++ engineer_dashboard.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="ThemeBucket">
+    <link rel="shortcut icon" href="images/favicon.png">
+
+    <title>DLSU IT Asset Management</title>
+
+    <link href="bs3/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-reset.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.css" rel="stylesheet" />
+
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/style-responsive.css" rel="stylesheet" />
+</head>
+
+<body>
+
+    <section id="container">
+        <!--header start-->
+        <header class="header fixed-top clearfix">
+            <!--logo start-->
+            <div class="brand">
+
+                <a href="#" class="logo">
+                    Welcome Engineer!
+                </a>
+            </div>
+
+            <div class="nav notify-row" id="top_menu">
+
+            </div>
+
+        </header>
+        <!--header end-->
+        <?php include 'engineer_navbar.php' ?>
+
+        <!--main content-->
+        <section id="main-content">
+            <section class="wrapper">
+                <!-- page start-->
+
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="col-sm-12">
+
+
+                            <div class="row">
+                                <a href="#">
+                                    <div class="col-md-2">
+                                        <div class="mini-stat clearfix">
+                                            <span class="mini-stat-icon orange"><i class="fa fa-gavel"></i></span>
+                                            <div class="mini-stat-info"><br>
+                                                <span>3200</span>
+                                                Unresolved
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+
+                                <a href="#">
+                                    <div class="col-md-2">
+                                        <div class="mini-stat clearfix">
+                                            <span class="mini-stat-icon orange"><i class="fa fa-clock-o"></i></span>
+                                            <div class="mini-stat-info">
+                                                <span>22,450</span>
+                                                Overdue
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+
+                                <a href="#">
+                                    <div class="col-md-2">
+                                        <div class="mini-stat clearfix">
+                                            <span class="mini-stat-icon pink"><i class="fa fa-clock-o"></i></span>
+                                            <div class="mini-stat-info">
+                                                <span>34,320</span>
+                                                Due Today
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+
+                                <a href="#">
+                                    <div class="col-md-2">
+                                        <div class="mini-stat clearfix">
+                                            <span class="mini-stat-icon green"><i class="fa fa-eye"></i></span>
+                                            <div class="mini-stat-info">
+                                                <span>32720</span>
+                                                Open
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+
+
+                                <a href="#">
+                                    <div class="col-md-2">
+                                        <div class="mini-stat clearfix">
+                                            <span class="mini-stat-icon orange"><i class="fa fa-times-circle-o"></i></span>
+                                            <div class="mini-stat-info">
+                                                <span>32720</span>
+                                                On Hold
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+
+
+                                <a href="#">
+                                    <div class="col-md-2">
+                                        <div class="mini-stat clearfix">
+                                            <span class="mini-stat-icon orange"><i class="fa fa-question"></i></span>
+                                            <div class="mini-stat-info">
+                                                <span>32720</span>
+                                                Unassigned
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+
+
+
+
+
+
+
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="col-sm-12">
+
+                            <section class="panel">
+                                <header class="panel-heading">
+                                    Recent Tickets
+                                </header>
+                                <div class="panel-body">
+                                    <section id="unseen">
+                                        <table class="table table-bordered table-striped table-condensed table-hover" id="ctable">
+                                            <thead>
+                                                <tr>
+                                                    <th>#</th>
+                                                    <th>Title</th>
+                                                    <th>Updated</th>
+                                                    <th>Action</th>
+                                                    <th>Status</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <td>1</td>
+                                                    <td>Need Help Here</td>
+                                                    <td>10/9/18</td>
+                                                    <td><span class="label label-success">Solved</span></td>
+                                                    <td><span class="label label-danger">Closed</span></td>
+                                                </tr>
+
+                                                <tr>
+                                                    <td>2</td>
+                                                    <td>Need Help Here</td>
+                                                    <td>10/9/18</td>
+                                                    <td><span class="label label-warning">Un-answered</span></td>
+                                                    <td><span class="label label-success">Opened</span></td>
+                                                </tr>
+
+                                                <tr>
+                                                    <td>3</td>
+                                                    <td>Need Help Here</td>
+                                                    <td>10/9/18</td>
+                                                    <td><span class="label label-danger">New Ticket</span></td>
+                                                    <td><span class="label label-success">Opened</span></td>
+                                                </tr>
+
+                                            </tbody>
+                                        </table>
+                                    </section>
+                                </div>
+                            </section>
+                        </div>
+                    </div>
+                </div>
+                <!-- page end-->
+            </section>
+        </section>
+        <!--main content end-->
+
+    </section>
+
+    <!-- WAG GALAWIN PLS LANG -->
+
+    <!--Core js-->
+    <script src="js/jquery.js"></script>
+    <script src="bs3/js/bootstrap.min.js"></script>
+    <script class="include" type="text/javascript" src="js/jquery.dcjqaccordion.2.7.js"></script>
+    <script src="js/jquery.scrollTo.min.js"></script>
+    <script src="js/jQuery-slimScroll-1.3.0/jquery.slimscroll.js"></script>
+    <script src="js/jquery.nicescroll.js"></script>
+
+    <script>
+        $('#ctable').on('dblclick', function() {
+            window.location.replace("helpdesk_view_ticket.php");
+        })
+    </script>
+
+    <!--common script init for all pages-->
+    <script src="js/scripts.js"></script>
+
+</body>
+
+</html>
