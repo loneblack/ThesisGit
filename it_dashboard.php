1ff68542015c93d06fa9b0ec3e7cb17c11ab889f
Marvin Lao <marvinlao619@gmail.com>
2018-10-16 14:02:13 +0800

    lol ok na ata to??

--- it_dashboard.php
+++ it_dashboard.php
@@ -14,6 +14,11 @@
     <link href="bs3/css/bootstrap.min.css" rel="stylesheet">
     <link href="css/bootstrap-reset.css" rel="stylesheet">
     <link href="font-awesome/css/font-awesome.css" rel="stylesheet" />
+    <link rel="stylesheet" href="js/morris-chart/morris.css">
+    <!--dynamic table-->
+    <link href="js/advanced-datatable/css/demo_page.css" rel="stylesheet" />
+    <link href="js/advanced-datatable/css/demo_table.css" rel="stylesheet" />
+    <link rel="stylesheet" href="js/data-tables/DT_bootstrap.css" />
 
     <link href="css/style.css" rel="stylesheet">
     <link href="css/style-responsive.css" rel="stylesheet" />
@@ -45,9 +50,142 @@
             <section class="wrapper">
                 <!-- page start-->
 
-                <div class="row">
+                <div class="col-sm-12">
                     <div class="col-sm-12">
-                        <div class="col-sm-12">
+
+                        <div class="row">
+
+                            <a href="it_inventory.php">
+                                <div class="col-md-3">
+                                    <div class="mini-stat clearfix">
+                                        <span class="mini-stat-icon green"><i class="fa fa-barcode"></i></span>
+                                        <div class="mini-stat-info">
+                                            <span>32,000</span>
+                                            Total Assets
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+
+                            <a href="it_software.php">
+                                <div class="col-md-3">
+                                    <div class="mini-stat clearfix">
+                                        <span class="mini-stat-icon orange"><i class="fa fa-save"></i></span>
+                                        <div class="mini-stat-info">
+                                            <span>22,450</span>
+                                            Total Licenses
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+
+                            <a href="it_inventory.php">
+                                <div class="col-md-3">
+                                    <div class="mini-stat clearfix">
+                                        <span class="mini-stat-icon pink"><i class="fa fa-keyboard-o"></i></span>
+                                        <div class="mini-stat-info">
+                                            <span>34,320</span>
+                                            Total Accessories
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+
+                            <a href="it_inventory.php">
+                                <div class="col-md-3">
+                                    <div class="mini-stat clearfix">
+                                        <span class="mini-stat-icon green"><i class="fa fa-files-o"></i></span>
+                                        <div class="mini-stat-info">
+                                            <span>32720</span>
+                                            Total Consumables
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+
+
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <section class="panel">
+                                        <header class="panel-heading">
+                                            Recent Activities
+                                            <span class="tools pull-right">
+                                                <a href="javascript:;" class="fa fa-chevron-down"></a>
+                                            </span>
+                                        </header>
+                                        <div class="panel-body">
+                                            <div class="adv-table">
+                                                <table class="display table table-bordered table-striped" id="dynamic-table">
+                                                    <thead>
+                                                        <tr>
+                                                            <th>Date</th>
+                                                            <th>Action</th>
+                                                            <th>Item</th>
+                                                            <th class="hidden-phone">To</th>
+                                                            <th class="hidden-phone">Remarks</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        <tr class="gradeX">
+                                                            <td>October 15, 2018 9:53PM</td>
+                                                            <td>Checked In</td>
+                                                            <td>Win 95+</td>
+                                                            <td>Mr. Allan Peter</td>
+                                                            <td>Registered to PC</td>
+                                                        </tr>
+                                                        <tr class="gradeX">
+                                                            <td>October 15, 2018 9:53PM</td>
+                                                            <td>Checked In</td>
+                                                            <td>Win 95+</td>
+                                                            <td>Mr. Allan Peter</td>
+                                                            <td>Registered to PC</td>
+                                                        </tr>
+                                                        <tr class="gradeX">
+                                                            <td>October 15, 2018 9:53PM</td>
+                                                            <td>Checked In</td>
+                                                            <td>Win 95+</td>
+                                                            <td>Mr. Allan Peter</td>
+                                                            <td>Registered to PC</td>
+                                                        </tr>
+                                                        <tr class="gradeX">
+                                                            <td>October 15, 2018 9:53PM</td>
+                                                            <td>Checked In</td>
+                                                            <td>Win 95+</td>
+                                                            <td>Mr. Allan Peter</td>
+                                                            <td>Registered to PC</td>
+                                                        </tr>
+                                                        <tr class="gradeX">
+                                                            <td>October 15, 2018 9:53PM</td>
+                                                            <td>Checked In</td>
+                                                            <td>Win 95+</td>
+                                                            <td>Mr. Allan Peter</td>
+                                                            <td>Registered to PC</td>
+                                                        </tr>
+                                                        <tr class="gradeX">
+                                                            <td>October 15, 2018 9:53PM</td>
+                                                            <td>Checked In</td>
+                                                            <td>Win 95+</td>
+                                                            <td>Mr. Allan Peter</td>
+                                                            <td>Registered to PC</td>
+                                                        </tr>
+                                                    </tbody>
+                                                    <tfoot>
+                                                        <tr>
+                                                            <th>Date</th>
+                                                            <th>Action</th>
+                                                            <th>Item</th>
+                                                            <th class="hidden-phone">To</th>
+                                                            <th class="hidden-phone">Remarks</th>
+                                                        </tr>
+                                                    </tfoot>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </section>
+                                </div>
+                            </div>
+
+
                         </div>
                     </div>
                 </div>
@@ -60,7 +198,6 @@
 
     <!-- WAG GALAWIN PLS LANG -->
 
-    <!--Core js-->
     <script src="js/jquery.js"></script>
     <script src="bs3/js/bootstrap.min.js"></script>
     <script class="include" type="text/javascript" src="js/jquery.dcjqaccordion.2.7.js"></script>
@@ -68,9 +205,18 @@
     <script src="js/jQuery-slimScroll-1.3.0/jquery.slimscroll.js"></script>
     <script src="js/jquery.nicescroll.js"></script>
 
-
+    <!--dynamic table-->
+    <script type="text/javascript" language="javascript" src="js/advanced-datatable/js/jquery.dataTables.js"></script>
+    <script type="text/javascript" src="js/data-tables/DT_bootstrap.js"></script>
     <!--common script init for all pages-->
     <script src="js/scripts.js"></script>
+
+    <script src="js/morris-chart/morris.js"></script>
+    <script src="js/morris-chart/raphael-min.js"></script>
+    <script src="js/morris.init.js"></script>
+
+    <!--dynamic table initialization -->
+    <script src="js/dynamic_table_init.js"></script>
 
 </body>
 
