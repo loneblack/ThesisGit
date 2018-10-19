f54f4a04e31cc9fe5d43edf6de5a1199335494b6
loneblack <41181113+loneblack@users.noreply.github.com>
2018-10-19 17:26:03 +0800

    Added Backend for Service reqeust form

--- requestor_service_request_form_DB.php
+++ requestor_service_request_form_DB.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+require_once("db/mysql_connect.php");
 
 $serviceType = $_POST['serviceType'];
 $others = $_POST['others'];
@@ -7,6 +8,14 @@ $details = $_POST['details'];
 $dateNeeded = $_POST['dateNeeded'];
 $endDate = $_POST['endDate'];
 
-echo $serviceType." ".$others." ".$details." ".$dateNeeded." ".$endDate;
+$header =  $_SESSION['previousPage'];
 
+$sql = "INSERT INTO `thesis`.`service` (`details`, `dateNeed`, `endDate`, `UserID`, `serviceType`, `others`, `status`)
+                                VALUES ('{$details}', '{$dateNeeded}', '{$endDate}', '1', '{$serviceType}', '{$others}', '10');";//status is set to 10 for pending status
+$result0 = mysqli_query($dbc, $sql);
+
+$message = "Form submitted!";
+$_SESSION['submitMessage'] = $message;
+
+header('Location: '.$header);
 ?>
