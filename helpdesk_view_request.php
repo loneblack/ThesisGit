7535d2b9771bab6ebe4f137b82fe74f8964bf1de
Marvin Lao <marvinlao619@gmail.com>
2018-10-22 15:00:09 +0800

    ops

--- helpdesk_view_request.php
+++ helpdesk_view_request.php
@@ -65,7 +65,7 @@
                                         <div class="modal-content">
                                             <div class="modal-header">
                                                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                                <h4 class="modal-title">Modal Header</h4>
+                                                <h4 class="modal-title">Create a Ticket</h4>
                                             </div>
                                             <div class="modal-body">
                                                 <div class="form">
