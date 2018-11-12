37b38c243b54b194abba03fde45edaab217faec3
Marvin Lao <marvinlao619@gmail.com>
2018-11-12 18:33:31 +0800

    made it inventory report as discussed

---
+++ it_inventory_report.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="ThemeBucket">
+    <link rel="shortcut icon" href="images/favicon.png">
+
+    <title>DLSU IT Asset Management</title>
+
+    <link href="bs3/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-reset.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.css" rel="stylesheet" />
+    <link rel="stylesheet" href="js/data-tables/DT_bootstrap.css" />
+    <link href="js/advanced-datatable/css/demo_table.css" rel="stylesheet" />
+
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/style-responsive.css" rel="stylesheet" />
+</head>
+
+<body>
+
+    <section id="container">
+        <!--header start-->
+        <header class="header fixed-top clearfix">
+            <!--logo start-->
+            <div class="brand">
+
+                <a href="#" class="logo">
+                    Welcome IT Officer!
+                </a>
+            </div>
+
+            <div class="nav notify-row" id="top_menu">
+
+            </div>
+
+        </header>
+        <!--header end-->
+        <?php include 'it_navbar.php' ?>
+
+        <!--main content-->
+        <section id="main-content">
+            <section class="wrapper">
+                <!-- page start-->
+
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="col-sm-12">
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <section class="panel">
+                                        <header class="panel-heading">
+                                            Inventory Report
+
+                                        </header>
+                                        <div class="panel-body">
+                                            <div class="row">
+                                                <div class="col-sm-12">
+                                                    <section class="panel">
+                                                        <div class="panel-body">
+                                                            <div class="adv-table">
+                                                                <table class="display table table-bordered table-striped" id="dynamic-table">
+                                                                    <thead>
+                                                                        <tr>
+                                                                            <th>Asset Category</th>
+                                                                            <th>Action</th>
+                                                                            <th>Quantity</th>
+                                                                            <th>Date</th>
+                                                                            
+                                                                        </tr>
+                                                                    </thead>
+                                                                    <tbody>
+                                                                        <tr>
+                                                                            <td>Computer</td>
+                                                                            <td>Purchased</td>
+                                                                            <td>5</td>
+                                                                            <td>12-23-2018 9:00:00 AM</td>
+                                                                        </tr>
+                                                                        <tr>
+                                                                            <td>Computer</td>
+                                                                            <td>Disposed</td>
+                                                                            <td>5</td>
+                                                                            <td>12-23-2018 9:00:00 AM</td>
+                                                                        </tr>
+                                                                        <tr>
+                                                                            <td>Repaired</td>
+                                                                            <td>Purchased</td>
+                                                                            <td>5</td>
+                                                                            <td>12-23-2018 9:00:00 AM</td>
+                                                                        </tr>
+                                                                        <tr>
+                                                                            <td>Computer</td>
+                                                                            <td>Purchased</td>
+                                                                            <td>5</td>
+                                                                            <td>12-23-2018 9:00:00 AM</td>
+                                                                        </tr>
+                                                                    </tbody>
+                                                                </table>
+                                                            </div>
+                                                        </div>
+                                                    </section>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </section>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- page end-->
+            </section>
+        </section>
+        <!--main content end-->
+
+    </section>
+    
+
+    <!-- WAG GALAWIN PLS LANG -->
+    
+    <script>
+		function addRowHandlers() {
+			var table = document.getElementById("dynamic-table");
+			var rows = table.getElementsByTagName("tr");
+			for (i = 1; i < rows.length; i++) {
+				var currentRow = table.rows[i];
+				var createClickHandler = function(row) {
+					return function() {
+						var cell = row.getElementsByTagName("td")[0];
+						var idx = cell.textContent;
+						
+                        window.location.replace("it_inventory_specific.php?=");
+						
+					};
+				};
+				currentRow.onclick = createClickHandler(currentRow);
+			}
+		}
+		window.onload = addRowHandlers();
+	</script>
+    
+    <!--Core js-->
+    <script src="js/jquery.js"></script>
+    <script src="bs3/js/bootstrap.min.js"></script>
+    <script class="include" type="text/javascript" src="js/jquery.dcjqaccordion.2.7.js"></script>
+    <script src="js/jquery.scrollTo.min.js"></script>
+    <script src="js/jQuery-slimScroll-1.3.0/jquery.slimscroll.js"></script>
+    <script src="js/jquery.nicescroll.js"></script>
+    <script type="text/javascript" language="javascript" src="js/advanced-datatable/js/jquery.dataTables.js"></script>
+    <script type="text/javascript" src="js/data-tables/DT_bootstrap.js"></script>
+    <script src="js/dynamic_table_init.js"></script>
+
+    <!--common script init for all pages-->
+    <script src="js/scripts.js"></script>
+    
+    
+
+</body>
+
+</html>
