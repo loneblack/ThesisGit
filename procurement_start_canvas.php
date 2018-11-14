fb096b8a52773167668361c6e400475834dc6173
Marvin Lao <marvinlao619@gmail.com>
2018-11-14 10:02:54 +0800

    add associate product to supplier sa procuremennt start canvas

--- procurement_start_canvas.php
+++ procurement_start_canvas.php
@@ -57,6 +57,23 @@
     <link href="css/bootstrap-reset.css" rel="stylesheet">
     <link href="font-awesome/css/font-awesome.css" rel="stylesheet" />
 
+    <link rel="stylesheet" href="css/bootstrap-switch.css" />
+    <link rel="stylesheet" type="text/css" href="js/bootstrap-fileupload/bootstrap-fileupload.css" />
+    <link rel="stylesheet" type="text/css" href="js/bootstrap-wysihtml5/bootstrap-wysihtml5.css" />
+    <link rel="stylesheet" type="text/css" href="js/bootstrap-datepicker/css/datepicker.css" />
+    <link rel="stylesheet" type="text/css" href="js/bootstrap-timepicker/compiled/timepicker.css" />
+    <link rel="stylesheet" type="text/css" href="js/bootstrap-colorpicker/css/colorpicker.css" />
+    <link rel="stylesheet" type="text/css" href="js/bootstrap-daterangepicker/daterangepicker-bs3.css" />
+    <link rel="stylesheet" type="text/css" href="js/bootstrap-datetimepicker/css/datetimepicker.css" />
+    <link rel="stylesheet" type="text/css" href="js/jquery-multi-select/css/multi-select.css" />
+    <link rel="stylesheet" type="text/css" href="js/jquery-tags-input/jquery.tagsinput.css" />
+    <link href="js/advanced-datatable/css/demo_page.css" rel="stylesheet" />
+    <link href="js/advanced-datatable/css/demo_table.css" rel="stylesheet" />
+    <link rel="stylesheet" href="js/data-tables/DT_bootstrap.css" />
+
+    <link rel="stylesheet" type="text/css" href="js/select2/select2.css" />
+
+    <!-- Custom styles for this template -->
     <link href="css/style.css" rel="stylesheet">
     <link href="css/style-responsive.css" rel="stylesheet" />
 </head>
@@ -151,9 +168,22 @@
 																	<div class="form-group">
 																		<label>Address</label>
 																		<div name="address" id="address">
-																			<textarea style="resize: none" class="form-control" rows="6" name="address" required></textarea>
+																			<textarea style="resize: none" class="form-control" rows="3" name="address" required></textarea>
 																		</div>
 																	</div>
+                                                                    <div class="form-group">
+                                                                        <label class="control-label col-md-3"><br>Associate Product to Supplier</label>
+                                                                        <div class="col-md-9">
+                                                                            <br>
+                                                                            ** Place Product on Right To Associate
+                                                                            <select name="country" class="multi-select" multiple="" id="my_multi_select3">
+                                                                                <option>Lenovo BHT 940</option>
+                                                                                <option>ROG GTX-1940</option>
+                                                                                <option>Logitech Mouse</option>
+                                                                                <option>Samsung Galaxy S7 Edge</option>
+                                                                            </select>
+                                                                        </div>
+                                                                    </div>
 																</form>
 															</div>
 															<div class="modal-footer">
@@ -314,15 +344,42 @@
 
     <!--Core js-->
     <script src="js/jquery.js"></script>
-    <script src="bs3/js/bootstrap.min.js"></script>
-    <script class="include" type="text/javascript" src="js/jquery.dcjqaccordion.2.7.js"></script>
-    <script src="js/jquery.scrollTo.min.js"></script>
-    <script src="js/jQuery-slimScroll-1.3.0/jquery.slimscroll.js"></script>
-    <script src="js/jquery.nicescroll.js"></script>
+<script src="js/jquery-1.8.3.min.js"></script>
+<script src="bs3/js/bootstrap.min.js"></script>
+<script src="js/jquery-ui-1.9.2.custom.min.js"></script>
+<script class="include" type="text/javascript" src="js/jquery.dcjqaccordion.2.7.js"></script>
+<script src="js/jquery.scrollTo.min.js"></script>
+<script src="js/jQuery-slimScroll-1.3.0/jquery.slimscroll.js"></script>
+<script src="js/jquery.nicescroll.js"></script>
+<script src="js/jquery.nicescroll.js"></script>
+
+<script src="js/bootstrap-switch.js"></script>
+
+<script type="text/javascript" src="js/bootstrap-wysihtml5/wysihtml5-0.3.0.js"></script>
+<script type="text/javascript" src="js/bootstrap-wysihtml5/bootstrap-wysihtml5.js"></script>
+<script type="text/javascript" src="js/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
+<script type="text/javascript" src="js/bootstrap-datetimepicker/js/bootstrap-datetimepicker.js"></script>
+<script type="text/javascript" src="js/bootstrap-colorpicker/js/bootstrap-colorpicker.js"></script>
+<script type="text/javascript" src="js/bootstrap-timepicker/js/bootstrap-timepicker.js"></script>
+<script type="text/javascript" src="js/jquery-multi-select/js/jquery.multi-select.js"></script>
+<script type="text/javascript" src="js/jquery-multi-select/js/jquery.quicksearch.js"></script>
+<script type="text/javascript" language="javascript" src="js/advanced-datatable/js/jquery.dataTables.js"></script>
+<script type="text/javascript" src="js/data-tables/DT_bootstrap.js"></script>
+<script type="text/javascript" src="js/bootstrap-inputmask/bootstrap-inputmask.min.js"></script>
+
+<script src="js/jquery-tags-input/jquery.tagsinput.js"></script>
+
+<script src="js/select2/select2.js"></script>
+<script src="js/select-init.js"></script>
 
 
-    <!--common script init for all pages-->
-    <script src="js/scripts.js"></script>
+<!--common script init for all pages-->
+<script src="js/scripts.js"></script>
+
+<script src="js/toggle-init.js"></script>
+
+<script src="js/advanced-form.js"></script>
+<script src="js/dynamic_table_init.js"></script>
 
     <script type="text/javascript">
         // Shorthand for $( document ).ready()
