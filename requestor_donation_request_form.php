ba212b822bec41d81d6db9739635e5729460eb47
Marvin Lao <marvinlao619@gmail.com>
2018-11-14 15:37:18 +0800

    pinapagawa ni austin

--- requestor_donation_request_form.php
+++ requestor_donation_request_form.php
@@ -57,15 +57,36 @@
                                         <div class="form" method="post">
                                             <form class="cmxform form-horizontal " id="signupForm" method="get" action="">
                                                 <div class="form-group ">
-                                                    <label for="organization" class="control-label col-lg-3">Office/Department/School Organization</label>
+                                                    <label class="control-label col-lg-3">Affiliation</label>
                                                     <div class="col-lg-6">
-                                                        <input type="text" name="organization" class="form-control m-bot15" required>
+                                                        <select class="form-control" id="ddl1" onchange="configureDropDownLists(this,document.getElementById('ddl2'))">
+                                                            <option>Select Affiliation</option>
+                                                            <option value="Office">Office</option>
+                                                            <option value="Department">Department</option>
+                                                            <option value="Organization">School Organization</option>
+                                                        </select>
                                                     </div>
                                                 </div>
+
+
+
+                                                <div class="form-group" id="office">
+                                                    <label class="control-label col-lg-3"></label>
+                                                    <div class="col-lg-6">
+                                                        <select class="form-control" id="ddl2">
+                                                        </select>
+                                                    </div>
+                                                </div>
+
+
+
+
+
+
                                                 <div class="form-group ">
                                                     <label for="number" class="control-label col-lg-3">Contact No.</label>
                                                     <div class="col-lg-6">
-                                                        <input class="form-control" rows="5" name="details" style="resize:none" type="text" required></input>
+                                                        <input class="form-control" rows="5" name="details" style="resize:none" type="text" required>
                                                     </div>
                                                 </div>
                                                 <div class="form-group ">
@@ -74,43 +95,43 @@
                                                         <input class="form-control" id="dateNeeded" name="dateNeeded" type="datetime-local" />
                                                     </div>
                                                 </div>
-												<div class="form-group ">
+                                                <div class="form-group ">
                                                     <label for="purpose" class="control-label col-lg-3">Purpose</label>
                                                     <div class="col-lg-6">
                                                         <input class="form-control" id="purpose" name="purpose" type="text" />
                                                     </div>
                                                 </div>
-												
+
                                                 <hr>
-												<div class="container-fluid">
-													<h4>Requested Equipment</h4>
-													
-													<table style="width:670px" class="table table-bordered table-striped table-condensed table-hover" id="tblCustomers" align="center" cellpadding="0" cellspacing="0" border="1">
-														<thead>
-															<tr>
-																<th style="width:500px">Equipment</th>
-																<th style="width:150px">Quantity</th>
-																<th></th>
-															</tr>
-														</thead>
-														<tbody>
-														</tbody>
-														<tfoot>
-															<tr>
-																<td>
+                                                <div class="container-fluid">
+                                                    <h4>Requested Equipment</h4>
+
+                                                    <table style="width:670px" class="table table-bordered table-striped table-condensed table-hover" id="tblCustomers" align="center" cellpadding="0" cellspacing="0" border="1">
+                                                        <thead>
+                                                            <tr>
+                                                                <th style="width:500px">Equipment</th>
+                                                                <th style="width:150px">Quantity</th>
+                                                                <th></th>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                        </tbody>
+                                                        <tfoot>
+                                                            <tr>
+                                                                <td>
                                                                     <select class="form-control" id="txtName">
                                                                         <option>Select</option>
                                                                     </select>
                                                                 </td>
-																<td><input class="form-control" type="number" min="1" id="txtCountry" /></td>
-																<td style="text-align:center"><input class="btn btn-primary" type="button" onclick="Add()" value="Add" /></td>
-															</tr>
-														</tfoot>
-													</table>
-												</div>
-												<hr>
+                                                                <td><input class="form-control" type="number" min="1" id="txtCountry" /></td>
+                                                                <td style="text-align:center"><input class="btn btn-primary" type="button" onclick="Add()" value="Add" /></td>
+                                                            </tr>
+                                                        </tfoot>
+                                                    </table>
+                                                </div>
+                                                <hr>
                                                 <div class="container-fluid">
-    												
+
                                                     <div class="form-group">
                                                         <div class="col-lg-offset-3 col-lg-6">
                                                             <button class="btn btn-primary" type="submit">Save</button>
@@ -132,20 +153,20 @@
         <!--main content end-->
 
     </section>
-	
-	<!--Script for table -->
-	<script type="text/javascript">
-        window.onload = function () {
+
+    <!--Script for table -->
+    <script type="text/javascript">
+        window.onload = function() {
             //Build an array containing Customer records.
             var customers = new Array();
-            
- 
+
+
             //Add the data rows.
             for (var i = 0; i < customers.length; i++) {
                 AddRow(customers[i][0], customers[i][1]);
             }
         };
- 
+
         function Add() {
             var txtName = document.getElementById("txtName");
             var txtCountry = document.getElementById("txtCountry");
@@ -153,36 +174,36 @@
             txtName.value = "";
             txtCountry.value = "";
         };
- 
+
         function Remove(button) {
             //Determine the reference of the Row using the Button.
             var row = button.parentNode.parentNode;
             var name = row.getElementsByTagName("TD")[0].innerHTML;
             if (confirm("Remove: " + name)) {
- 
+
                 //Get the reference of the Table.
                 var table = document.getElementById("tblCustomers");
- 
+
                 //Delete the Table row using it's Index.
                 table.deleteRow(row.rowIndex);
             }
         };
- 
+
         function AddRow(name, country) {
             //Get the reference of the Table's TBODY element.
             var tBody = document.getElementById("tblCustomers").getElementsByTagName("TBODY")[0];
- 
+
             //Add Row.
             row = tBody.insertRow(-1);
- 
+
             //Add Name cell.
             var cell = row.insertCell(-1);
             cell.innerHTML = name;
- 
+
             //Add Country cell.
             cell = row.insertCell(-1);
             cell.innerHTML = country;
- 
+
             //Add Button cell.
             cell = row.insertCell(-1);
             var btnRemove = document.createElement("INPUT");
@@ -195,16 +216,54 @@
             cell.appendChild(btnRemove);
         }
 
-        function getRooms(val){
+        function getRooms(val) {
             $.ajax({
-            type:"POST",
-            url:"requestor_getRooms.php",
-            data: 'buildingID='+val,
-            success: function(data){
-                $("#FloorAndRoomID").html(data);
+                type: "POST",
+                url: "requestor_getRooms.php",
+                data: 'buildingID=' + val,
+                success: function(data) {
+                    $("#FloorAndRoomID").html(data);
 
                 }
             });
+        }
+
+        function configureDropDownLists(ddl1, ddl2) {
+            var colours = ['Select Office', 'IT Office', 'Accounting Office', 'ABC Office'];
+            var shapes = ['Select Department', 'Accountancy', 'Behavioral Sciences', 'Biology'];
+            var names = ['Select Organization', 'ENGLICOM', 'LSCS', 'USG'];
+
+            switch (ddl1.value) {
+                case 'Office':
+                    ddl2.options.length = 0;
+                    for (i = 0; i < colours.length; i++) {
+                        createOption(ddl2, colours[i], colours[i]);
+                    }
+                    break;
+                case 'Department':
+                    ddl2.options.length = 0;
+                    for (i = 0; i < shapes.length; i++) {
+                        createOption(ddl2, shapes[i], shapes[i]);
+                    }
+                    break;
+                case 'Organization':
+                    ddl2.options.length = 0;
+                    for (i = 0; i < names.length; i++) {
+                        createOption(ddl2, names[i], names[i]);
+                    }
+                    break;
+                default:
+                    ddl2.options.length = 0;
+                    break;
+            }
+
+        }
+
+        function createOption(ddl, text, value) {
+            var opt = document.createElement('option');
+            opt.value = value;
+            opt.text = text;
+            ddl.options.add(opt);
         }
     </script>
 
