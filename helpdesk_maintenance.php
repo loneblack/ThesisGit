2d57d730f0e321f8a0a6b11020e4d87e8e24f6a9
fredpurisimaIT <gian_purisima@dlsu.edu.ph>
2019-02-20 16:00:12 +0800

    backend done

--- helpdesk_maintenance.php
+++ helpdesk_maintenance.php
@@ -1,4 +1,41 @@
 <!DOCTYPE html>
+<?php
+	session_start();
+	require_once('db/mysql_connect.php');
+	
+	if(isset($_POST['save'])){
+		$building=$_POST['building'];
+		$roomtype=$_POST['roomtype'];
+		$engineer=$_POST['engineer'];
+		
+		$mi = new MultipleIterator();
+		
+		$mi->attachIterator(new ArrayIterator($building));
+		$mi->attachIterator(new ArrayIterator($roomtype));
+		$mi->attachIterator(new ArrayIterator($engineer));
+		
+		foreach($mi as $value){
+			list($building, $roomtype, $engineer) = $value;
+			
+			//Check if an engineer assignment is exist
+			$queryIsEnAssEx = "SELECT count(*) as `isExist` FROM thesis.engineer_assignment where BuildingID='{$building}' and roomtypeID='{$roomtype}'";
+			$resultIsEnAssEx  = mysqli_query($dbc, $queryIsEnAssEx);
+			$rowIsEnAssEx = mysqli_fetch_array($resultIsEnAssEx, MYSQLI_ASSOC);
+			
+			if($rowIsEnAssEx['isExist']=='0' && $engineer!='0'){
+				//INSERT TO ENGINEER ASSIGNMENT
+				$queryIsEnAss = "INSERT INTO `thesis`.`engineer_assignment` (`BuildingID`, `roomtypeID`, `employeeID`) VALUES ('{$building}', '{$roomtype}', '{$engineer}');";
+				$resultIsEnAss  = mysqli_query($dbc, $queryIsEnAss);
+			}
+			elseif($rowIsEnAssEx['isExist']=='1' && $engineer!='0'){
+				//UPDATE TO ENGINEER ASSIGNMENT
+				$queryUpEnAss = "UPDATE `thesis`.`engineer_assignment` SET `employeeID`='{$engineer}' WHERE `BuildingID`='{$building}' and `roomtypeID`='{$roomtype}';";
+				$resultUpEnAss  = mysqli_query($dbc, $queryUpEnAss);
+			}
+		}
+		$_SESSION['submitMessage'] = "Success! The maintenance team list has been updated.";
+	}
+?>
 <html lang="en">
 
 <head>
@@ -47,7 +84,14 @@
         <section id="main-content">
             <section class="wrapper">
                 <!-- page start-->
-
+				<?php
+					if (isset($_SESSION['submitMessage'])){
+                        echo "<div class='alert alert-success'>
+                                {$_SESSION['submitMessage']}
+							  </div>";
+                        unset($_SESSION['submitMessage']);
+                    }
+				?>
                 <div class="row">
                     <div class="col-sm-12">
 
@@ -62,8 +106,8 @@
                                     <div class="col-sm-12">
                                         <section class="panel">
                                             <div class="panel-body">
-                                                <form method="post">
-                                                    <button class="btn btn-success" type="submit">Save</button><br><br>
+                                                <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                                                    <button class="btn btn-success" type="submit" name="save">Save</button><br><br>
                                                     <div class="adv-table">
                                                         <table class="display table table-bordered table-striped" id="dynamic-table">
                                                             <thead>
@@ -75,16 +119,52 @@
                                                                 </tr>
                                                             </thead>
                                                             <tbody>
-                                                                <tr>
-                                                                    <td style="display: none"></td>
-                                                                    <td class="">Gokongwei</td>
-                                                                    <td class="">Classroom</td>
-                                                                    <td class="col-lg-5"> 
-                                                                        <select class="form-control">
-                                                                            <option value="0">Select Engineer</option>
-                                                                        </select>
-                                                                    </td>
-                                                                </tr>
+																<?php
+																	//GET LIST OF BUILDINGS AND ROOM TYPES TO BE ASSIGNED TO AN ENGINEER
+																	$queryGetListBuil = "SELECT b.BuildingID, b.name, r.id, r.roomtype FROM building b JOIN floorandroom f ON b.BuildingID = f.BuildingID
+																																					   JOIN ref_roomtype r ON f.roomtype = r.id
+																																					   group by f.roomtype,b.BuildingID 
+																																					   order by b.BuildingID asc";
+																	$resultGetListBuil = mysqli_query($dbc, $queryGetListBuil);
+																	while($rowGetListBuil = mysqli_fetch_array($resultGetListBuil, MYSQLI_ASSOC)){
+																		//GET CURRENT ENGINEER OF A GIVEN BUILDING AND ROOMTYPE
+																		$queryGetCurEng = "SELECT * FROM thesis.engineer_assignment where BuildingID='{$rowGetListBuil['BuildingID']}' and roomtypeID='{$rowGetListBuil['id']}'";
+																		$resultGetCurEng = mysqli_query($dbc, $queryGetCurEng);
+																		$rowGetCurEng = mysqli_fetch_array($resultGetCurEng, MYSQLI_ASSOC);
+																		echo "<tr>
+																			<input type='hidden' name='building[]' value='{$rowGetListBuil['BuildingID']}'>
+																			<input type='hidden' name='roomtype[]' value='{$rowGetListBuil['id']}'>
+																			<td style='display: none'></td>
+																			<td>{$rowGetListBuil['name']}</td>
+																			<td>{$rowGetListBuil['roomtype']}</td>
+																			<td class='col-lg-5'> 
+																			<select class='form-control' name='engineer[]'>
+																				<option value='0'>Select Engineer</option>";
+																				
+																				//GET LIST OF ENGINEERS
+																				$queryGetListEng = "SELECT * FROM thesis.employee e join user u on e.UserID=u.UserID where u.userType='4'";
+																				$resultGetListEng = mysqli_query($dbc, $queryGetListEng);
+																				while($rowGetListEng = mysqli_fetch_array($resultGetListEng, MYSQLI_ASSOC)){
+																					if($rowGetCurEng['employeeID']==$rowGetListEng['employeeID']){
+																						echo "<option value='{$rowGetListEng['employeeID']}' selected>{$rowGetListEng['name']}</option>";
+																					}
+																					else{
+																						echo "<option value='{$rowGetListEng['employeeID']}'>{$rowGetListEng['name']}</option>";
+																					}
+																					
+																				}
+																			echo "
+																			</select>
+																			</td>
+																		</tr>";
+																	}
+																
+																
+																
+																
+																
+																?>
+                                                                
                                                             </tbody>
                                                         </table>
                                                     </div>
