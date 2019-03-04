b6eac8badfbccf2e5dd0f764385b539ec4655af7
loneblack <41181113+loneblack@users.noreply.github.com>
2019-03-04 18:00:48 +0800

    Database for service unit and backend for view ticket repair opened

--- engineer_view_ticket_repair_opened.php
+++ engineer_view_ticket_repair_opened.php
@@ -4,6 +4,7 @@
 session_start();
 $userID = $_SESSION['userID'];
 $id = $_GET['id'];
+$_SESSION['previousPage'] = "engineer_view_ticket_repair_opened.php?id={$id}";
 require_once("db/mysql_connect.php");
 
 $query =  "SELECT *, t.status AS 'status', s.status AS 'statusDescription' FROM thesis.ticket t JOIN ref_ticketstatus s ON t.status = s.ticketID  WHERE t.ticketID = {$id};";
@@ -36,98 +37,6 @@ while ($row = mysqli_fetch_array($result2, MYSQLI_ASSOC)){
 }
 
 ?>
-<?php
-// Insertion to ticket
-    
-    if(isset($_POST['submit'])){
-		
-        $count = $_POST['count'];
-        if(empty($_POST['count'])){
-            $count = 0;
-        }
-        $status=$_POST['status'];
-        $priority=$_POST['priority'];
-        $currDate=date("Y-m-d H:i:s");
-		
-		
-		//GET SERVICEID
-		$queryServID="SELECT * FROM thesis.ticket where ticketID='{$id}'";
-		$resultServID=mysqli_query($dbc,$queryServID);
-		$rowServID=mysqli_fetch_array($resultServID,MYSQLI_ASSOC);
-
-
-        //Updating asset status
-        $counter = 0;
-        $assets=$_POST['assetID'];
-        foreach($assets as $asset){
-
-            $assetStatus = $_POST['assetStatus'.$counter];
-            $queryAssetStatus="UPDATE `thesis`.`asset` SET `assetStatus` = '{$assetStatus}' WHERE (`assetID` = '{$asset}');";
-            $resultAssetStatus=mysqli_query($dbc,$queryAssetStatus);
-            $counter++;
-        }
-
-        //Update Comment and assignee
-        $comment=$_POST['comment'];
-        $assigneeUserID = $_POST['escalateUserID'];
-        $queryUpdate="UPDATE `thesis`.`ticket` SET `assigneeUserID` = '{$assigneeUserID}',`comment` = '{$comment}' WHERE (`ticketID` = '{$id}');";
-        $resultUpdate=mysqli_query($dbc,$queryUpdate);
-		
-		
-		//Request for parts code
-		if(!empty($_POST['quantity0'])){
-            $i = 0;
-            while($i <= $count){
-
-                $cat = $_POST['category'.$i];
-                $qty = $_POST['quantity'.$i];
-                $specs = $_POST['specification'.$i];
-
-                $queryReqPart="INSERT INTO `thesis`.`requestparts` ( `serviceID`, `assetModelID`, `quantity`, `specifications`, `received`) VALUES ('{$serviceID}', '{$cat}' ,'{$qty}' ,'{$specs}', '0')";
-                $resultReqPart=mysqli_query($dbc,$queryReqPart);
-
-                $i++;
-            }
-
-                $querya="UPDATE `thesis`.`ticket` 
-                SET `status` = '6',
-                WHERE (`ticketID` = '{$id}');";
-                $resulta=mysqli_query($dbc,$querya);  
-		}
-        
-		//Check if all assets are repaired
-		
-		//GET QTY of Assets of a Ticket
-		$queryTicketed="SELECT count(ticketID) as `numAssets`, count(IF(checked = 1, ticketID, null)) as `repairedAssets` FROM thesis.ticketedasset where ticketID='{$id}'";
-		$resultTicketed=mysqli_query($dbc,$queryTicketed);
-		$rowTicketed=mysqli_fetch_array($resultTicketed,MYSQLI_ASSOC);
-		
-		//GET QTY of Assets of a SERVICE
-		$queryService="SELECT count(ta.ticketID) as `numAssets`, count(IF(checked = 1, ta.ticketID, null)) as `repairedAssets` FROM thesis.ticketedasset ta join ticket t on ta.ticketID=t.ticketID where t.service_id='{$rowServID['service_id']}'";
-		$resultService=mysqli_query($dbc,$queryService);
-		$rowService=mysqli_fetch_array($resultService,MYSQLI_ASSOC);
-		
-		//UPDATE SERVICE STATUS (STILL NEEDS TO BE FIXED)
-		if($rowService['numAssets']==$rowService['repairedAssets']){
-			$queryComp="UPDATE `thesis`.`service` SET `steps`='11' WHERE `id`='{$rowServID['service_id']}'";
-			$resultComp=mysqli_query($dbc,$queryComp);
-		}
-		
-		
-		//UPDATE TICKET STATUS 
-		if($rowTicketed['numAssets']==$rowTicketed['repairedAssets']){
-			$queryTickUp="UPDATE `thesis`.`ticket` SET `status`='7',`dateClosed` = '{$currDate}' WHERE `ticketID`='{$id}'";
-			$resultTickUp=mysqli_query($dbc,$queryTickUp);
-		}
-		
-        $message = "Ticket Updated!".$count;
-        $_SESSION['submitMessage'] = $message;
-        }
-        
-    //}
-    
-?>
-
 <head>
     <meta charset="utf-8">
 
@@ -468,7 +377,7 @@ while ($row = mysqli_fetch_array($result2, MYSQLI_ASSOC)){
                                                 </select>
                                             </td>
                                             <td>
-                                                <input class='form-control' name='sppecification0'>
+                                                <input class='form-control' name='specification0'>
                                             </td>
                                             <td>
                                                 <select class="form-control" id="deliveryStatus" name="deliveryStatus">
@@ -478,6 +387,58 @@ while ($row = mysqli_fetch_array($result2, MYSQLI_ASSOC)){
                                             </td>
                                             <td style="text-align:center" width='10px'><button class="btn btn-primary" type="button" onclick="addTest(1)">Add</button></td>
                                         </tr>
+
+                                        <?php 
+
+                                        $sql = "SELECT * FROM thesis.requestParts WHERE serviceID = '{$serviceID}';";
+                                        $result = mysqli_query($dbc, $sql);
+                                        while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
+                                        {
+
+                                            $delivered = ""; 
+                                            $pending = "";
+                                            if($row['received'] == 1){
+                                                $delivered = "selected";
+                                            }
+                                            else{
+                                                $pending = "selected";
+                                            }
+                                            echo
+                                            "<tr>
+                                                <td>
+                                                    <input type='number' min='0' step='1' class='form-control' name='quantity' value =  '{$row['quantity']}'readonly>
+                                                </td>
+                                                <td width='300'>
+                                                    <select class='form-control' name = 'category' readonly>";
+                                                        
+
+                                                            $sql = "SELECT * FROM thesis.ref_assetcategory WHERE assetCategoryID = '{$row['assetCategoryID']}';";
+
+                                                            $result = mysqli_query($dbc, $sql);
+
+                                                            while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
+                                                            {
+                                                                echo "<option value ={$row['assetCategoryID']}>";
+                                                                echo "{$row['name']}</option>";
+                                                            }
+
+                                                        
+                                            echo    
+                                                    "</select>
+                                                </td>
+                                                <td>
+                                                    <input class='form-control' name='specification' value= '{$row['specifications']}' readonly>
+                                                </td>
+                                                <td>
+                                                    <select class='form-control' id='deliveryStatus' name='deliveryStatus' readonly>
+                                                        <option value='0'".$pending.">Pending</option>
+                                                        <option value='1'".$delivered.">Delivered</option>
+                                                    </select>
+                                                </td>
+                                                <td></td>
+                                            </tr>";
+                                        }
+                                        ?>
                                     </tbody>
                                 </table>
                                 <input style="display: none" type="number" id="count" name = "count">
