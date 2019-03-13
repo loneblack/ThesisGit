6104ae48ba63090a8f83d27fdbfae4a9c418e8f9
loneblack <41181113+loneblack@users.noreply.github.com>
2019-03-13 13:51:24 +0800

    Update it_request_service_unit.php

--- it_request_service_unit.php
+++ it_request_service_unit.php
@@ -55,7 +55,7 @@ require_once("db/mysql_connect.php");
 
                 <div class="row">
                     <div class="col-sm-12">
-                        <div class="col-sm-9">
+                        <div class="col-sm-12">
                             <section class="panel">
                                 <header style="padding-bottom:20px" class="panel-heading wht-bg">
                                     <?php
@@ -83,6 +83,10 @@ require_once("db/mysql_connect.php");
                                                 <strong>me</strong>
                                             </div>
                                             <div class="col-md-4">
+                                                <button style="float:right" class="btn btn-warning">Pending</button>
+                                                <br>
+                                                <br>
+                                                <br>
                                                 <h5>Date Created:
                                                 </h5>
                                             </div>
@@ -118,89 +122,6 @@ require_once("db/mysql_connect.php");
                                 </div>
                             </section>
 
-                        </div>
-
-
-                        <div class="col-sm-3">
-
-                            <section class="panel">
-                                <div class="panel-body">
-                                    <ul class="nav nav-pills nav-stacked labels-info ">
-                                        <li>
-                                            <h4>Properties</h4>
-                                        </li>
-                                    </ul>
-                                    <div class="form">
-
-                                        <div class="form-group ">
-                                            <div class="form-group ">
-                                                <label for="category" class="control-label col-lg-4">Category</label>
-                                                <div class="col-lg-8">
-                                                    <select class="form-control m-bot15" disabled>
-                                                        <option selected="selected">Repair</option>
-                                                        <option>Repair</option>
-                                                    </select>
-                                                </div>
-                                            </div>
-
-                                            <label for="status" class="control-label col-lg-4">Status</label>
-                                            <div class="col-lg-8">
-                                                <select class="form-control m-bot15" name="status" readonly>
-                                                    <option value='1' <?php if($status=='1' ) echo "selected" ;?> >Open</option>
-                                                    <option value='2' <?php if($status=='2' ) echo "selected" ;?> >Assigned</option>
-                                                    <option value='3' <?php if($status=='3' ) echo "selected" ;?> >In Progress</option>
-                                                    <option value='4' <?php if($status=='4' ) echo "selected" ;?> >Transferred</option>
-                                                    <option value='5' <?php if($status=='5' ) echo "selected" ;?> >Escalated</option>
-                                                    <option value='6' <?php if($status=='6' ) echo "selected" ;?> >Waiting For Parts</option>
-                                                    <option value='7' <?php if($status=='7' ) echo "selected" ;?> >Closed</option>
-                                                </select>
-                                            </div>
-                                        </div>
-
-                                        <div class="form-group ">
-                                            <label for="priority" class="control-label col-lg-4">Priority</label>
-                                            <div class="col-lg-8">
-                                                <select class="form-control m-bot15" name="priority" readonly>
-                                                    <option value="Low">Low</option>
-                                                    <option value="Medium">Medium</option>
-                                                    <option value="High">High</option>
-                                                    <option value="Urgent">Urgent</option>
-                                                </select>
-                                            </div>
-                                        </div>
-
-                                        <div class="form-group ">
-                                            <label for="assign" class="control-label col-lg-4">Escalate To</label>
-                                            <div class="col-lg-8">
-                                                <select class="form-control m-bot15" name="escalateUserID" id='escalateUser' disabled>
-                                                    <option value='<?php echo $userID;?>'>None</option>
-                                                    <?php
-                                                            $query3="SELECT u.UserID,CONCAT(Convert(AES_DECRYPT(lastName,'Fusion')USING utf8),', ',Convert(AES_DECRYPT(firstName,'Fusion')USING utf8)) as `fullname` FROM thesis.user u join thesis.ref_usertype rut on u.userType=rut.id where rut.description='Engineer';";
-                                                            $result3=mysqli_query($dbc,$query3);
-                                                                    
-                                                            while($row3=mysqli_fetch_array($result3,MYSQLI_ASSOC)){
-                                                                //if($assigneeUserID == $row3['UserID']){
-                                                                    //echo "<option value='{$row3['UserID']}'>{$row3['fullname']}</option>";
-                                                                //}
-																if($assigneeUserID != $row3['UserID']){
-                                                                   echo "<option value='{$row3['UserID']}'>{$row3['fullname']}</option>";
-                                                                }    
-                                                            }   
-                                                        ?>
-                                                </select>
-                                            </div>
-                                        </div>
-
-                                        <div class="form-group">
-                                            <label class="control-label col-lg-4">Due Date</label>
-                                            <div class="col-lg-8">
-                                                <input class="form-control form-control-inline input-medium default-date-picker" size="10" type="text" value="<?php echo $dueDate;?>" disabled>
-                                            </div>
-                                        </div>
-
-                                    </div>
-                                </div>
-                            </section>
                         </div>
 
                         <div class="col-sm-12">
