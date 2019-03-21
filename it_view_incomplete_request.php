36fb93bd5fe5a2cc504b877d98b7ebb7c1b91e8b
fredpurisimaIT <32771993+fredpurisimaIT@users.noreply.github.com>
2019-03-21 11:23:43 +0800

    Fixed old replenish backend

--- it_view_incomplete_request.php
+++ it_view_incomplete_request.php
@@ -79,8 +79,8 @@ $_SESSION['previousPage2']="it_view_incomplete_request.php?requestID=".$requestI
                                             <div class="col-md-4 col-sm-4 pull-left">
                                                 <a href="it_add_brand.php" target="_blank"><button class="btn btn-primary" type="button">Add Brand</button></a>
                                                 <a href="it_add_product.php" target="_blank"><button class="btn btn-info" type="button">Add Product</button></a>
-                                                <h4>Status:</h4>
-                                                <h2>Incomplete</h2>
+                                                <!--<h4>Status:</h4>
+                                                <h2>Incomplete</h2>-->
                                             </div>
                                             <div class="col-md-4 col-sm-5 pull-right">
                                                 <div class="row">
@@ -106,7 +106,7 @@ $_SESSION['previousPage2']="it_view_incomplete_request.php?requestID=".$requestI
                                                     <th>Quantity</th>
                                                     <th>Category</th>
                                                     <th>Specifications</th>
-                                                    <th>Purpose</th>
+                                                    <th>Asset Description</th>
                                                 </tr>
                                             </thead>
                                             <tbody>
