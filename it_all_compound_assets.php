f3ea24d19d7d7ff7cec3fa97c2bf11fb64af36db
marvinlao <33211945+marvinlao@users.noreply.github.com>
2019-04-02 09:31:23 +0800

    push

--- it_all_compound_assets.php
+++ it_all_compound_assets.php
@@ -70,7 +70,7 @@
                                 <div class="col-sm-12">
                                     <section class="panel">
                                         <header class="panel-heading">
-                                            All Asset List
+                                            All Compound Asset List
 
                                         </header>
                                         <div class="panel-body">
