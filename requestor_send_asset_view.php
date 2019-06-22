53372ef797e6d56ed04ff3788d44599a00af8066
sia661 <siajohannes1@gmail.com>
2019-06-22 17:57:18 +0800

    test

---
+++ requestor_send_asset_view.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="ThemeBucket">
+    <link rel="shortcut icon" href="images/favicon.png">
+
+    <title>DLSU IT Asset Management</title>
+
+    <link href="bs3/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-reset.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.css" rel="stylesheet" />
+    <link rel="stylesheet" href="js/morris-chart/morris.css">
+    <!--dynamic table-->
+    <link href="js/advanced-datatable/css/demo_page.css" rel="stylesheet" />
+    <link href="js/advanced-datatable/css/demo_table.css" rel="stylesheet" />
+    <link rel="stylesheet" href="js/data-tables/DT_bootstrap.css" />
+
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/style-responsive.css" rel="stylesheet" />
+</head>
+
+<body>
+
+    <section id="container">
+        <!--header start-->
+        <header class="header fixed-top clearfix">
+            <!--logo start-->
+            <div class="brand">
+
+                <a href="#" class="logo">
+                    <img src="images/dlsulogo.png" alt="" width="200px" height="40px">
+                </a>
+            </div>
+
+            <div class="nav notify-row" id="top_menu">
+
+            </div>
+
+        </header>
+        <!--header end-->
+        <?php include 'it_navbar.php' ?>
+
+        <!--main content-->
+        <section id="main-content">
+            <section class="wrapper">
+                <!-- page start-->
+
+                <div class="col-sm-12">
+                    <div class="col-sm-12">
+
+                        <div class="row">
+
+
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <section class="panel">
+                                        <header class="panel-heading">
+                                            Send Borrowed Assets
+                                            <span class="tools pull-right">
+                                                <a href="javascript:;" class="fa fa-chevron-down"></a>
+                                            </span>
+                                        </header>
+                                        <div class="panel-body">
+                                            
+                                            <div class="adv-table">
+                                                <table class="display table table-bordered table-striped" id="dynamic-table">
+                                                    <thead>
+                                                        <tr>
+                                                            <th></th>
+                                                            <th>Property Code</th>
+                                                            <th>Brand</th>
+                                                            <th>Model</th>
+                                                            <th>Deployment Date</th>
+                                                            <th>Expected Return Date</th>
+                                                            <th>Status</th>
+                                                            <th>Comments</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        <tr>
+                                                            <th><input type="checkbox" readonly></th>
+                                                            <th>123 Testing</th>
+                                                            <th>Samsung</th>
+                                                            <th>Model</th>
+                                                            <th>5/24/2019</th>
+                                                            <th>6/22/2019</th>
+                                                            <th>
+                                                                <select class="form-control" readonly>
+                                                                    <option>Select</option>
+                                                                    <option>Working</option>
+                                                                    <option>Damaged</option>
+                                                                </select>
+                                                            </th>
+                                                            <th>
+                                                                <input class="form-control" type="text">
+                                                            </th>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                        <div style="padding-left:10px; padding-bottom:5px">
+                                            <button class="btn btn-success">Checkin</button>
+                                            <a href="it_bulk_checkin.php"><button class="btn btn-danger">Back</button></a>
+                                        </div>
+                                    </section>
+                                </div>
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
+                <!-- page end-->
+            </section>
+        </section>
+        <!--main content end-->
+
+    </section>
+
+    <!-- WAG GALAWIN PLS LANG -->
+    <script>
+        function addRowHandlers() {
+            var table = document.getElementById("dynamic-table");
+            var rows = table.getElementsByTagName("tr");
+            for (i = 1; i < rows.length; i++) {
+                var currentRow = table.rows[i];
+                var createClickHandler = function(row) {
+                    return function() {
+                        var cell = row.getElementsByTagName("td")[0];
+                        var idx = cell.textContent;
+
+                        window.location.href = "it_user_assets.php?=";
+
+                    };
+                };
+                currentRow.onclick = createClickHandler(currentRow);
+            }
+        }
+        window.onload = addRowHandlers();
+    </script>
+
+    <script src="js/jquery.js"></script>
+    <script src="bs3/js/bootstrap.min.js"></script>
+    <script class="include" type="text/javascript" src="js/jquery.dcjqaccordion.2.7.js"></script>
+    <script src="js/jquery.scrollTo.min.js"></script>
+    <script src="js/jQuery-slimScroll-1.3.0/jquery.slimscroll.js"></script>
+    <script src="js/jquery.nicescroll.js"></script>
+
+    <!--dynamic table-->
+    <script type="text/javascript" language="javascript" src="js/advanced-datatable/js/jquery.dataTables.js"></script>
+    <script type="text/javascript" src="js/data-tables/DT_bootstrap.js"></script>
+    <!--common script init for all pages-->
+    <script src="js/scripts.js"></script>
+
+    <script src="js/morris-chart/morris.js"></script>
+    <script src="js/morris-chart/raphael-min.js"></script>
+    <script src="js/morris.init.js"></script>
+
+    <!--dynamic table initialization -->
+    <script src="js/dynamic_table_init.js"></script>
+
+</body>
+
+</html>
